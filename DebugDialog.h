b2920664df5558e58e60bb8aaa535f6fb7df1fd7
Damian Wójcik <macmacroll@gmail.com>
2020-03-29 19:17:33 +0200

    sadwbsdfbsbsdb

--- DebugDialog.h
+++ DebugDialog.h
@@ -4,10 +4,18 @@
 #include <qwidget.h>
 #include <qgridlayout.h>
 #include <qtextedit.h>
+#include <qgridlayout.h>
+#include <qchart.h>
+#include <qchartview.h>
 
 class DebugDialog : public QDialog{
 	Q_OBJECT
 public:
+	QChartView* view = nullptr;
+	QGridLayout* layout = nullptr;
+	QPushButton* button = nullptr;
+	QChart* chart1 = nullptr;
+	QChart* chart2 = nullptr;
 	QGridLayout* debugLayout;
 	QTextEdit* debugTextEdit;
 	QString debugText;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include "Laboratory.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 	QApplication a(argc, argv);
 	Laboratoria w;
 	w.show();
